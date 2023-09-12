dfd125d4a0761dffe79de95e344bd131bb75e3a1
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-09-12 12:24:15 +0700

    Update class.recipe-menu.php

--- cpt/class.recipe-menu.php
+++ cpt/class.recipe-menu.php
@@ -2,41 +2,41 @@
 
 # register new post type
 class RecipeMenu extends \NEONBaseCPT\PostType {
-  
+
 	public function __construct() {
 		parent::__construct( [
-      # post type id. MUST unique
+			# post type id. MUST unique
 			'id'            => 'resep',
-      
-      # title post type
+
+			# title post type
 			'title'         => 'Resep',
 
-      # 'title', 'editor', 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', and 'post-formats'
+			# 'title', 'editor', 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', and 'post-formats'
 			'supports'      => [ 'title', 'thumbnail' ],
-      
-      # @see https://developer.wordpress.org/reference/functions/register_post_type/#menu_position             
+
+			# @see https://developer.wordpress.org/reference/functions/register_post_type/#menu_position             
 			'menu_position' => 25,
 
-      # @see https://developer.wordpress.org/resource/dashicons/
+			# @see https://developer.wordpress.org/resource/dashicons/
 			'menu_icon'     => 'dashicons-food',
 
-      # true: gutenberg editor, false: classic editor
+			# true: gutenberg editor, false: classic editor
 			'show_in_rest'  => false,
 
-      # enable mode archive for custom post type ex: https://domain.com/resep
+			# enable mode archive for custom post type ex: https://domain.com/resep
 			'has_archive'   => false,
 
-      # adding taxonomy like category or tags
+			# adding taxonomy like category or tags
 			'group'         => [
 				[
 					'id'                => 'resep-category',
 					'title'             => 'Category',
-                        
-          # show_admin_column: true show taxonomy in table list. default: false
+
+					# show_admin_column: true show taxonomy in table list. default: false
 					'show_admin_column' => true,
-                        
-          # hierarchical: true show like category, false like tags. default: false
-					'hierarchical'      => true, 
+
+					# hierarchical: true show like category, false like tags. default: false
+					'hierarchical'      => true,
 				],
 				[
 					'id'                => 'hashtag',

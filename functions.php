193c84a3ce076a8fc017631b4474e991ccc395e0
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2023-09-12 16:37:42 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,3 +2,23 @@
 
 require_once __DIR__ . '/cpt/class.base-cpt.php';
 require_once __DIR__ . '/cpt/class.recipe-menu.php';
+
+function the_current_taxonomy($taxonomy) {
+	global $post;
+	$taxonomies = get_the_terms($post->ID, $taxonomy);
+
+	if ($taxonomies && !is_wp_error($taxonomies)) {
+		$taxonomy_links = array();
+		foreach ($taxonomies as $taxonomy) {
+			$taxonomy_links[] = '<a href="' . get_term_link($taxonomy) . '">' . $taxonomy->name . '</a>';
+		}
+		echo implode(' ', $taxonomy_links);
+	}
+}
+
+/**
+ * contoh dipasang di single.php
+ * if ( function_exists('the_current_taxonomy') && is_singular('resep') ) {
+ *   	the_current_taxonomy('hashtag');
+ * }
+ */
